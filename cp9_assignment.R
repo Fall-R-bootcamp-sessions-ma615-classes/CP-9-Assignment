94e2880a063055ae82bfe41cfa213e761fe86fc4
aidanohara1 <aohara1@bu.edu>
2023-08-24 15:33:28 -0400

    temp and instruct

---
+++ cp9_assignment.R
@@ -0,0 +1,18 @@
+### YOUR NAME HERE ###
+
+# CP 9 Assignment         #
+#  Probability Simulation #
+
+set.seed(08242023) # KEEP THIS THE SAME, FOR THE AUTOGRADER
+
+# exercise 1
+battingSeries <- function(bProbs){
+  
+}
+
+# exercise 2
+diceRollin <- function(diceProbs, sums = 10, numDice = 4) {
+  
+}
+
+
